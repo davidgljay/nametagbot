7bc64fb75e21ad2c542a301b561f92faca37e140
David Jay <davidgljay@gmail.com>
2018-05-23 15:53:00 -0700

    Adding server side config variables.

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,25 @@
 const functions = require('firebase-functions')
 const db = require('./db')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   db.collection('test').doc('test').set({stuff: 'andthings'})
-//   response.send('Hello from Firebase! Testnig data write')
-// })
+const env = runtimeConfig.getVariable('slack_token')
 
 /*
 * Function for handling Slack events
 */
 
-exports.events = functions.https.onRequest((req, res) => {
+exports.events = functions.https.onRequest((req, res) => env.then(env => {
   res.writeHead(200, {'Content-Type': 'application/json'})
+  
+  if (req.body.token !== env.slack_token) {
+    console.error('Incorrect Slack token received with request')
+    res.end()
+    return
+  }
+  
   switch(req.body.type) {
   case url_verification:
     res.send({challenge: req.body.challenge})
     break
   }
-  //TODO: Parse by type
   //TODO: Call appropriate controller functions
-})
+}))
