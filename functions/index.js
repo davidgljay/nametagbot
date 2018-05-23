06c6cb2a3156f4524a8b84fb5acdecd0a308c708
David Jay <davidgljay@gmail.com>
2018-05-23 15:05:58 -0700

    Testign db write.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const db = require('./db')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+  db.collection('test').doc('test').set({stuff: 'andthings'})
+  response.send('Hello from Firebase! Testnig data write')
+})
