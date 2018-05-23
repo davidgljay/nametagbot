d9ff36ce3bae10cd29e1b85cf73e3e775b9ffebf
David Jay <davidgljay@gmail.com>
2018-05-23 15:46:53 -0700

    Handling Slack verification.

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,22 @@ const db = require('./db')
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  db.collection('test').doc('test').set({stuff: 'andthings'})
-  response.send('Hello from Firebase! Testnig data write')
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   db.collection('test').doc('test').set({stuff: 'andthings'})
+//   response.send('Hello from Firebase! Testnig data write')
+// })
+
+/*
+* Function for handling Slack events
+*/
+
+exports.events = functions.https.onRequest((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  switch(req.body.type) {
+  case url_verification:
+    res.send({challenge: req.body.challenge})
+    break
+  }
+  //TODO: Parse by type
+  //TODO: Call appropriate controller functions
 })
