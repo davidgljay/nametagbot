40b7f31dd21a7b03691ca1db15a27917f8fcbdbb
David Jay <davidgljay@gmail.com>
2018-05-24 19:54:00 -0700

    Fixed bugs to return challenge as expected.

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,14 @@ const events = require('./events')
 
 exports.events = functions.https.onRequest((req, res) => {
   if (req.method !== 'POST' || req.body.token !== env.bot.slack_token) {
-    console.log(req.method)
-    console.error('Incorrect Slack token received with request', env, req.body)
-    console.log(req.body.token, env.bot.slack_token, env.bot.slack_token === req.body.token)
+    console.error('Incorrect Slack token received with request', req.body.token)
     res.status(403)
     res.send('Not Authorized')
     res.end()
     return
   }
 
-  res.writeHead(200, {'Content-Type': 'application/json'})
+  res.setHeader('Content-Type','application/json')
 
   if (req.body.type === 'url_verification') {
     events.urlVerification(req, res)
