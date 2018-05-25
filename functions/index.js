47a50dc115742812518cb4bd67cb609aa6a1a8b7
David Jay <davidgljay@gmail.com>
2018-05-24 18:29:01 -0700

    Rejecting incorrectly formatted errors.

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,21 @@
 const functions = require('firebase-functions')
 const runtimeConfig = require('cloud-functions-runtime-config')
 const env = runtimeConfig.getVariable('slack_token')
-const events = require('../events')
+const events = require('./events')
 
 /*
 * Function for handling Slack events
 */
 
 exports.events = functions.https.onRequest((req, res) => env.then(env => {
-  res.writeHead(200, {'Content-Type': 'application/json'})
-
-  if (req.body.token !== env.slack_token) {
+  
+  if (req.body.token !== env.slack_token || req.status !== 'POST') {
     console.error('Incorrect Slack token received with request')
-    res.end()
+    res.status(403).send('Not Authorized')
     return
   }
+  
+  res.writeHead(200, {'Content-Type': 'application/json'})
 
   if (req.body.type === 'url_verification') {
     events.urlVerification(req, res)
