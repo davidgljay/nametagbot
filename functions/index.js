ad04ab86c6a7b5465ec1c141ca2fb5235de25bad
David Jay <davidgljay@gmail.com>
2018-05-24 19:35:54 -0700

    Switching env to firebase.config()

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,27 @@
 const functions = require('firebase-functions')
-const runtimeConfig = require('cloud-functions-runtime-config')
-const env = runtimeConfig.getVariable('slack_token')
+const env = functions.config()
 const events = require('./events')
 
 /*
 * Function for handling Slack events
 */
 
-exports.events = functions.https.onRequest((req, res) => env.then(env => {
-  
-  if (req.body.token !== env.slack_token || req.status !== 'POST') {
+exports.events = functions.https.onRequest((req, res) => {
+  if (req.status !== 'POST' || req.body.token !== env.slack_token) {
     console.error('Incorrect Slack token received with request')
-    res.status(403).send('Not Authorized')
+    res.status(403)
+    res.send('Not Authorized')
+    res.end()
     return
   }
-  
+
   res.writeHead(200, {'Content-Type': 'application/json'})
 
   if (req.body.type === 'url_verification') {
     events.urlVerification(req, res)
   }
 
+  res.end()
+
   // TODO: Call appropriate controller functions
-}))
+})
