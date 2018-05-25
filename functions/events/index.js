b4a15dac03e78bfbff92d3d75fd81226999f16de
David Jay <davidgljay@gmail.com>
2018-05-24 20:01:32 -0700

    Adding team join stub.

--- functions/events/index.js
+++ functions/events/index.js
@@ -1,5 +1,7 @@
-const urlVerification = require('./url_verification')
+const url_verification = require('./url_verification')
+const team_join = require('./team_join')
 
 module.exports = {
-  urlVerification
+  url_verification,
+  team_join
 }
--- functions/index.js
+++ functions/index.js
@@ -18,8 +18,12 @@ exports.events = functions.https.onRequest((req, res) => {
   res.setHeader('Content-Type','application/json')
 
   if (req.body.type === 'url_verification') {
-    events.urlVerification(req, res)
+    events[req.body.type](req, res)
+  } else if (req.body.type === 'event_callback') {
+    events[req.body.event.type](req, res)
   }
+  
+  
 
   res.end()
 
