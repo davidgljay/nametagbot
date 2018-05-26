0175e390bf51ed648c2ed6f8f25c903d955ce579
David Jay <davidgljay@gmail.com>
2018-05-26 15:50:36 -0700

    Expanding language.

--- functions/lang.js
+++ functions/lang.js
@@ -1,3 +1,16 @@
 module.exports = {
-  welcome: () => 'Welcome message!'
+  newMember: {
+    welcome: (teamName) => `Welcome to ${teamName}! Several people from this group have volunteered to help welcome new people like you, I'd love to connect you with one who matches your interests. Sound good?`,
+    nothanks: () => 'No problem, just type "join" if you change your mind.'
+  },
+  profile: {
+    background: () => 'Great! To get started, briefly share your background. Here\'s what other people from the community have said:',
+    bio: () => 'Cool, now give us a sense of what brings you here. Why did you decide to join this community? Here\'s what a few other people have said:'
+  },
+  greeter: {
+    recruit: () => 'Would you like to help orient new people to this channel?' +
+      'If you agree I\'ll ocassionally connect you with people who have just joined.',
+    nothanks: () => 'No problem! I won\'t bother you again. If you change your mind just type "join".',
+    thanks: () => 'Thanks! I\'ve added you to a special #greeters channel and will connect you with new people as they join.'
+  }
 }
