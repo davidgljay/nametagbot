c6f0ff71f9568766b1de6deb60e423faeba1958a
David Jay <davidgljay@gmail.com>
2018-05-27 14:43:20 -0700

    getting list of new members on registration.

--- functions/events/register.js
+++ functions/events/register.js
@@ -1,5 +1,5 @@
 const team = require('../models/team')
+const web = require('../slackapi')
 
-module.exports = (req, res) => team.create(
-  Object.assign({}, req.body, {id: req.body.team_id})
-)
+module.exports = (req, res) => team.create(Object.assign({}, req.body, {id: req.body.team_id}))
+  .then(() => web.users.list({token: req.body.token}))
--- functions/spec/register.spec.js
+++ functions/spec/register.spec.js
@@ -1,16 +1,31 @@
+
 const register = require('../events/register')
-jest.mock('../models/team')
 const team = require('../models/team')
+const slackapi = require('../slackapi')
+jest.mock('../models/team')
+jest.mock('../slackapi')
 
 describe('register', () => {
-  it('should save to the the database', () => {
-    const req = {
+  let req
+
+  beforeEach (() => {
+    req = {
       body: {
-        team_id: '12345'
+        team_id: '12345',
+        token: 'abcd'
       }
     }
 
+    team.create.mockReturnValue(Promise.resolve())
+  })
+
+  it('should save to the the database', () => {
     register(req)
-    expect(team.create.mock.calls[0][0]).toEqual({team_id: '12345', id: '12345'})
+    expect(team.create.mock.calls[0][0]).toEqual({team_id: '12345', id: '12345', token: 'abcd'})
+  })
+
+  it ('should get a list of current members', () => {
+    register(req)
+    expect(slackapi.users.list.mock.calls[0][0]).toEqual({token: 'abcd'})
   })
 })
