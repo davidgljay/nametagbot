03a919d6e86c294afb64adf721738fbc7fe34d2c
David Jay <davidgljay@gmail.com>
2018-05-30 23:33:17 -0700

    Initializing web api with bot token.

--- functions/slackapi.js
+++ functions/slackapi.js
@@ -2,6 +2,6 @@ const { WebClient } = require('@slack/client')
 const functions = require('firebase-functions')
 const env = functions.config()
 
-module.exports = new WebClient(env.slack.token)
+module.exports = new WebClient(env.slack.bot_token)
 
 module.exports.catchErr = msg => res => res.ok ? res : Promise.reject(new Error(msg, res.error))
