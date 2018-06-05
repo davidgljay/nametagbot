2c5a0a7934bd82cf5a808ab54f2a7da0b90e18bf
David Jay <davidgljay@gmail.com>
2018-06-05 10:59:14 -0700

    Switching environment vars to process.env.

--- slackapi.js
+++ slackapi.js
@@ -1,7 +1,5 @@
 const { WebClient } = require('@slack/client')
-const functions = require('firebase-functions')
-const env = functions.config()
 
-module.exports = new WebClient(env.slack.bot_token)
+module.exports = new WebClient(process.env.SLACK_BOT_TOKEN)
 
 module.exports.catchErr = msg => res => res.ok ? res : Promise.reject(new Error(msg, res.error))
--- templates.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'wecome': () => 'Welcome message'
-}
