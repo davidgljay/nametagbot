662269c6cb7fdf246b2b53bb619cd9f09265930c
David Jay <davidgljay@gmail.com>
2018-06-26 20:26:24 -0700

    Adding tests for registration.

--- events/register.js
+++ events/register.js
@@ -5,10 +5,7 @@ const lang = require('../lang')
 const {SLACK_CLIENT_ID, SLACK_CLIENT_SECRET} = process.env
 
 module.exports = (req, db) => slackapi.oauth.access({code: req.query.code, client_id: SLACK_CLIENT_ID, client_secret: SLACK_CLIENT_SECRET})
-  .then(teamInfo => {
-    console.log(teamInfo)
-    return team.create(Object.assign({}, teamInfo, {id: teamInfo.team_id, bot_access_token: teamInfo.bot.bot_access_token}))
-  })
+  .then(teamInfo => team.create(Object.assign({}, teamInfo, {id: teamInfo.team_id, bot_access_token: teamInfo.bot.bot_access_token})))
   .then(() => slackapi.users.list({token: req.body.token}))
   .then(users => Promise.all([
     users
